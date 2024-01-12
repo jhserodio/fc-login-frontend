d7710366181575e6096db8ffd250bd26b2054a30
João Henrique Serodio <joao.serodio@vnator.com>
2024-01-12 16:54:48 -0300

    chore: config js code formats - prettier - eslint (more rules) - editor config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,53 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'eslint-config-prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        paths: [
+          'src'
+        ],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx'
+        ]
+      }
+    }
+  },
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'prettier',
+    'import'
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': [
+      'error',
+      {
+        'vars': 'all',
+        'args': 'after-used',
+        'ignoreRestSiblings': true,
+        'argsIgnorePattern': '^_'
+      }
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error'] 
   },
 }
