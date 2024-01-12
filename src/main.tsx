a8a77efdd020ae918cc8b7c0fca8e32e2e3c1c32
João Henrique Serodio <joao.serodio@vnator.com>
2024-01-12 19:25:36 -0300

    feat: add theme variables - pure css variables - add resets - add some rules for fix cross-browser diferences

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import * as Sentry from '@sentry/react';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import App from './App.tsx';
-import './index.css';
+import './theme.css';
 import i18nInitConfig from './i18n/config.ts';
 
 i18n.use(initReactI18next).init(i18nInitConfig);
