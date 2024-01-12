3b95a19a8f41307cfeabb3ed9c824b0c1decfdd9
João Henrique Serodio <joao.serodio@vnator.com>
2024-01-12 18:19:00 -0300

    chore: tests configs - add jest - include an example test - update lint to tests

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/config/__mocks__/fileMock.js',
+    '^.+\\.(css|less)$': '<rootDir>/config/__mocks__/cssMock.js',
+  },
+};
